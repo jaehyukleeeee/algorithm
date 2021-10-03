b578fea839999022574aa8d8ed69d7e90f511575
Jaehyuk Lee <jaehyuk1209@gmail.com>
2021-10-04 03:37:30 +0900

    2번 문제

---
+++ inflearn/cpp-1/q2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, sum = 0;
+
+	cin >> a >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		sum += i;
+		if (i == b)
+		{
+			cout << i;
+		}
+		else
+		{
+			cout << i << " + ";
+		}
+	}
+	
+	cout << " = " << sum << endl;
+
+	return 0;
+}
