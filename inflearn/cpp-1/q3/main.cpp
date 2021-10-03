969017a2068173e7ea858dba5f3b5831d5302f9e
Jaehyuk Lee <jaehyuk1209@gmail.com>
2021-10-04 03:37:37 +0900

    3번 문제

---
+++ inflearn/cpp-1/q3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, sum = 1;
+
+	cin >> n;
+
+	cout << 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (n % i == 0 && n != i)
+		{
+			sum += i;
+			cout << " + " << i;
+		}
+	}
+
+	cout << " = " << sum << endl;
+
+	return 0;
+}
